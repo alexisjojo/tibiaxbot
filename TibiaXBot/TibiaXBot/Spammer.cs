6279ab49cff8e329835e1304a0d19a2921bb3f40
MoneyMakingAccount2 <moneymakingaccount2@gmail.com>
2008-08-18 17:45:17 +0000

    Added The GUI // Jesper

---
+++ TibiaXBot/TibiaXBot/Spammer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TibiaXBot
+{
+    public partial class Spammer : Form
+    {
+        public Spammer()
+        {
+            InitializeComponent();
+        }
+    }
+}
