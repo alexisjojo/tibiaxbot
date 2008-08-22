c84dffa0d692c630252ada9c194b70354dfa1d52
moneymakingaccount2@gmail.com <moneymakingaccount2@gmail.com@26b71f4e-bf54-0410-b107-6de5ef1e9b05>
2008-08-22 22:12:42 +0000

    Fixed the mapviewer, did wrong last time xD

---
+++ trunk/TibiaXBot/TibiaXBot/Mapviewer.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TibiaXBot
+{
+    partial class Mapviewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.mapViewer1 = new Tibia.Util.MapViewer();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(272, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // plus
+            // 
+            this.plus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.plus.FlatAppearance.BorderSize = 0;
+            this.plus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.plus.Location = new System.Drawing.Point(239, 12);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(42, 38);
+            this.plus.TabIndex = 5;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.minus.FlatAppearance.BorderSize = 0;
+            this.minus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.minus.Location = new System.Drawing.Point(239, 45);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(42, 38);
+            this.minus.TabIndex = 6;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // mapViewer1
+            // 
+            this.mapViewer1.Directory = "C:\\Documents and Settings\\JesperC#\\Application Data\\Tibia\\Automap\\";
+            this.mapViewer1.DrawCoors = true;
+            this.mapViewer1.DrawCrosshair = true;
+            this.mapViewer1.Location = new System.Drawing.Point(0, 1);
+            this.mapViewer1.Name = "mapViewer1";
+            this.mapViewer1.OpenTibiaMaps = false;
+            this.mapViewer1.Size = new System.Drawing.Size(233, 164);
+            this.mapViewer1.TabIndex = 8;
+            this.mapViewer1.Paint += new System.Windows.Forms.PaintEventHandler(this.mapViewer1_Paint);
+            // 
+            // Mapviewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(293, 167);
+            this.Controls.Add(this.mapViewer1);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Mapviewer";
+            this.Text = "Mapviewer";
+            this.Load += new System.EventHandler(this.Mapviewer_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private Tibia.Util.MapViewer mapViewer1;
+    }
+}
---
+++ trunk/TibiaXBot/TibiaXBot/Mapviewer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tibia;
+using Tibia.Objects;
+using Tibia.Constants;
+
+namespace TibiaXBot
+{
+    public partial class Mapviewer : Form
+    {
+        public Client client;
+        public Tibia.Util.MapViewer mv;
+        public Player player;
+
+        public Mapviewer(Client client)
+        {
+            InitializeComponent();
+            this.client = client;
+        }
+
+        private void Mapviewer_Load(object sender, EventArgs e)
+        {
+            player = client.GetPlayer();
+            mapViewer1.LoadMap();
+            mapViewer1.SetMapCenter(player.Location);
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            mapViewer1.Zoom(2.0);
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            mapViewer1.Zoom(0.5);
+        }
+
+
+
+        private void boxMapviewer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mapViewer1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
