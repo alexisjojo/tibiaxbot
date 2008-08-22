5314672dfb621b6a6012d6c3b4132590264f76d8
aluwama <aluwama@26b71f4e-bf54-0410-b107-6de5ef1e9b05>
2008-08-22 18:08:24 +0000

    //aluwama

---
+++ trunk/TibiaXBot/TibiaXBot.Updater/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TibiaXBot.Properties;
+using Tibia;
+using Tibia.Objects;
+
+
+namespace TibiaXBot.Updater
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
